50ec7312ac22a326e5b69babb6617536125a2fca
Sarah Jane West <4419364+sarad1p1ty@users.noreply.github.com>
2022-05-08 14:48:32 +0100

    Adding Bootstrap 5 with test index page

---
+++ gatsby-browser.ts
@@ -0,0 +1,5 @@
+//import './src/styles/custom.scss';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "@popperjs/core/dist/umd/popper.min.js";
---
+++ src/pages/TextInfo.tsx
@@ -0,0 +1,6 @@
+import * as React from "react"
+
+function TextInfo (props) {
+    return <div className="text text-info">{props.children}</div>
+}
+export default TextInfo; 
